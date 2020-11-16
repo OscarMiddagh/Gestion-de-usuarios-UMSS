2a9ef3823b98ddaa0e82c2703b0d8c90d30a7b7b
Ares22.2 <andre.arellano3@gmail.com>
2020-11-15 23:24:02 -0400

    propuesta para tablero y respuesta de solicitudes

--- frontend/src/componentes/Tablero-Solicitudes/Tablero-Solicitudes.jsx
+++ frontend/src/componentes/Tablero-Solicitudes/Tablero-Solicitudes.jsx
@@ -1,91 +1,236 @@
 //Campo para la historia Tablero de Solicitudes
-import React from 'react'
+import React from "react";
+//import "./App.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Table,
+  Button,
+  Container,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
 
-const TableroSolicitudes = () => {
-  return (
-    <div id="id"> 
-     <br></br>
-     <div>
-      <h6>SOLICITUDES DE CAMBIO DE ROL</h6>
-    </div>
-    <br></br>
- 
-    <div class="section slats" heigth="200px" width="500px"> 
-    <table  border="black" width="500px" >
-      <tr>
-     <ul> 
-    <li class="relative selfclear"> 
+const data = [
+  { id: 1, nombre: "Ramon", fecha: "11/12/20",rolA: "",rolS: "", mensajeS:"", comentario: "" },
+  { id: 2, nombre: "Pepe", fecha: "13/09/20", rolA: "",rolS: "", mensajeS:"", comentario:"" },
+  { id: 3, nombre: "Julio", fecha: "15/08/20", rolA: "",rolS:"", mensajeS:"",comentario:"" },
+];
 
-      <div class="selfclear" > 
-        <div class="meta unit size3of4"> 
-          <div class="lastUnit size1of1 !padding--lv0"> 
-            <span title="Success" class="freddicon tick-circle-large float-left hide-phone"></span> 
-            <h4>
-              <a href="#">saraisela@scdp.com</a>
-            </h4> 
-            <p class="dim-el">
-              <span role="presentation" class="swatch green show-phone" title="success"></span>Sara Isela
-            </p> 
-          </div> 
-        </div> 
-        <div class="meta-actions lastUnit size1of4 alignr"> 
-          <button href="#" class="button-small p0" title="Configure">Responder</button> 
-        </div> 
-      </div> 
+class App extends React.Component {
+  state = {
+    data: data,
+    modalActualizar: false,
+    modalInsertar: false,
+    form: {
+      id: "",
+      Nombre: "",
+      fecha: "",
+    },
+  };
 
+  mostrarModalActualizar = (dato) => {
+    this.setState({
+      form: dato,
+      modalActualizar: true,
+    });
+  };
 
-      
+  cerrarModalActualizar = () => {
+    this.setState({ modalActualizar: false });
+  };
 
-      <div class="selfclear"> 
-        <div class="meta unit size3of4"> 
-          <div class="lastUnit size1of1 !padding--lv0"> 
-            <span title="Success" class="freddicon tick-circle-large float-left hide-phone"></span> 
-            <h4>
-              <a href="#">elsapato@scdp.com</a>
-            </h4> 
-            <p class="dim-el">
-              <span role="presentation" class="swatch green show-phone" title="success"></span>Elsa Pato
-            </p> 
-          </div> 
-        </div> 
-        <div class="meta-actions lastUnit size1of4 alignr"> 
-          <button href="#" class="button-small p0" title="Configure">Responder</button> 
-        </div> 
-      </div> 
+  mostrarModalInsertar = () => {
+    this.setState({
+      modalInsertar: true,
+    });
+  };
 
-      <div class="selfclear"> 
-        <div class="meta unit size3of4"> 
-          <div class="lastUnit size1of1 !padding--lv0"> 
-            <span title="Success" class="freddicon tick-circle-large float-left hide-phone"></span> 
-            <h4>
-              <a href="#">elbasurita@scdp.com</a>
-            </h4> 
-            <p class="dim-el">
-              <span role="presentation" class="swatch green show-phone" title="success"></span>Elba Surita
-            </p> 
-          </div> 
-        </div> 
-        <div class="meta-actions lastUnit size1of4 alignr"> 
-          <button href="#" class="button-small p0" title="Configure">Responder</button> 
-        </div> 
-      </div> 
+  cerrarModalInsertar = () => {
+    this.setState({ modalInsertar: false });
+  };
 
+  aprobar = (dato) => {
+    
+  };
+  rechazar = (dato) =>{
 
-      
+  };
 
-    </li> 
-  </ul> 
-  </tr>
-  
-  </table>
-</div>
+  eliminar = (dato) => {
+    var opcion = window.confirm("Estás Seguro que deseas Eliminar el elemento "+dato.id);
+    if (opcion == true) {
+      var contador = 0;
+      var arreglo = this.state.data;
+      arreglo.map((registro) => {
+        if (dato.id == registro.id) {
+          arreglo.splice(contador, 1);
+        }
+        contador++;
+      });
+      this.setState({ data: arreglo, modalActualizar: false });
+    }
+  };
 
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-  
-</div>
+  render() {
+    
+    return (
+      <>
+        <Container>
+       
+          <Table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Fecha</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
 
+            <tbody>
+              {this.state.data.map((dato) => (
+                <tr key={dato.id}>
+                  <td>{dato.id}</td>
+                  <td>{dato.nombre}</td>
+                  <td>{dato.fecha}</td>
+                  <td>
+                    <Button
+                      color="primary"
+                      onClick={() => this.mostrarModalActualizar(dato)}
+                    >
+                      Responder
+                    </Button>{" "}
+                    <Button color="danger" onClick={()=> this.eliminar(dato)}>Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
 
-  )
+        <Modal isOpen={this.state.modalActualizar}>
+          <ModalHeader>
+           <div><h3>Responder</h3></div>
+          </ModalHeader>
+
+          <ModalBody>
+            <FormGroup>
+              <label>
+               Id:
+              </label>
+            
+              <input
+                className="form-control"
+                readOnly
+                type="text"
+                value={this.state.form.id}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Nombre: 
+              </label>
+              <input
+                className="form-control"
+                name="nombre"
+                type="text"
+                readOnly
+                value={this.state.form.nombre}
+              />
+            </FormGroup>
+            
+            <FormGroup>
+              <label>
+                Rol Actual: 
+              </label>
+              <input
+                className="form-control"
+                name="rolA"
+                type="text"
+                readOnly
+                value={this.state.form.rolA}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>
+                Rol Solicitado: 
+              </label>
+              <input
+                className="form-control"
+                name="rolS"
+                type="text"
+                readOnly
+                value={this.state.form.rolS}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>
+                fecha: 
+              </label>
+              <input
+                className="form-control"
+                name="fecha"
+                type="text"
+                readOnly
+                value={this.state.form.fecha}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>
+                Mensaje de solicitud: 
+              </label>
+              <input
+                className="form-control"
+                name="mensajeS"
+                type="text"
+                readOnly
+                value={this.state.form.mensajeS}
+              />
+            </FormGroup>
+            <FormGroup>
+              <label>
+                Comentario: 
+              </label>
+              <input
+                className="form-control"
+                name="comentario"
+                type="text"
+               onChange={this.handleChange}
+                
+              />
+            </FormGroup>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              color="primary"
+              onClick={() => this.responder(this.state.form)}
+            >
+              Aprobar
+            </Button>
+            <Button
+              color="danger"
+              onClick={() => this.cerrarModalActualizar()}
+            >
+              Rechazar
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
 }
-
-export default TableroSolicitudes
+export default App;
