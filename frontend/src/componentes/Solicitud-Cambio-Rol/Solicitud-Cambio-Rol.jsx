b81b51221cda240c4ca1be58a7616b602bfd61fe
Alex Castro <alexander72577@gmail.com>
2020-11-20 22:59:38 -0400

    Se arreglo la interfaz y se añadio mensaje de confirmacion

--- frontend/src/componentes/Solicitud-Cambio-Rol/Solicitud-Cambio-Rol.jsx
+++ frontend/src/componentes/Solicitud-Cambio-Rol/Solicitud-Cambio-Rol.jsx
@@ -1,21 +1,42 @@
 //campo para la historia de Solicitud de Cambio de Rol
 import React from 'react'
-
+import "./Solicitud-Cambio-Rol.css"
 const SolicitudCambioRol = () => {
+  const SendRequest = (event) =>{
+    event.preventDefault();
+    try {
+      //Aqui viene lo del axios.
+      alert("Solicitud Enviada");
+    } catch (error) {
+      alert("Solicitud no enviada, reintente por favor");
+    }
+    
+      
+      
+    }
+  
   return (
-    <div class="contenido">
-    <body>
+    
+    <form action="" align="center">
+      <div>
+    <body class="hello">
+    <br>
+        </br>
     <h3 align="center">SOLICITUD DE CAMBIO DE ROL</h3>
-    <h6>Motivo de solicitud:</h6>
-    <textarea>
-        Necesito tener mas permisos para trabajar
+    <br>
+    </br>
+    <br>
+    </br>
+    <h6 align="center">Motivo de solicitud:</h6>
+    <textarea  class="centro" placeholder="Ingrese su mensaje" rows="6" required>
+        
       </textarea>
       <br>
         </br>
-    <label for="cars">Roles disponibles:</label>
-    <form action="" method="post">
-        <select id="cars" name="cars">
-            <option value="admin">Admin</option>
+    <label for="cars" class="centro2">Roles disponibles:</label>
+    
+        <select id="cars" name="cars" class="centro3">
+            <option value="admin" >Admin</option>
             <option value="delivery">Delivery</option>
             <option value="vendedor">Vendedor</option>
             <option value="comprador">Comprador</option>
@@ -24,10 +45,16 @@ const SolicitudCambioRol = () => {
         </br>
         <br>
         </br>
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
-</body>
+        <div class="row"> 
+    <div class="col text-center"> 
+    <button class="button button-small button-light" onClick={SendRequest} value="Enviar">Enviar</button>
+    
+    </div> 
+</div> 
+        </body>
 </div>
+    </form>
+
   
   
   )
