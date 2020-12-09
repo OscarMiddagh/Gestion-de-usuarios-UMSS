24cc7f3fccb63730ff9554b7f78926d042f1115a
Andres Moscoso <andres221196@gmail.com>
2020-12-09 00:43:14 -0400

    Añadida la restriccion de tipo de caracteres al campo Nombre de Rol

--- frontend/src/componentes/Crear-Rol/CrearRol.jsx
+++ frontend/src/componentes/Crear-Rol/CrearRol.jsx
@@ -1,26 +1,37 @@
 //Campo para la historia Crear Rol
 import React from 'react'
+import { useState } from "react";
 import "./CrearRol.css"
-class CrearRol extends React.Component{
-    state = {
-        form: {
-            "rol": ""
-          }
-      };
-    render(){
-        return (
-          <div className="contenido" id="div"> <br/>
-              <h3 align="center">CREAR ROL</h3>
-              
-              <form align="center">
-              <div id="div">
-              <label for="rol" id="label">Nuevo Rol :</label>
-              <input id="rolText" className="text1" placeholder="Nombre de rol"  rows="3" name="rol" minLength={5} maxLength={20} />
-              </div>
-              <input type="submit" className="btn btn-primary" onClick={this.enviar} value="Agregar"/>
-              </form>     
+const CrearRol = () => {
+  const [datos, setDatos] = useState({
+    nombrerol: ''
+  })
+  const { nombrerol } = datos
+  const limitaralfabeticos = (e) => {
+    if (e.target.value.match("^[Ñña-zA-Z]*$") != null) {
+      setDatos({
+        ...datos,
+        [e.target.name]: e.target.value
+      })
+    }
+    else {
+      alert("Solo se aceptan caracteres alfabeticos");
+    }
+  }
+
+  return (
+    <div className="contenido" id="div"> <br />
+      <h3 align="center">CREAR ROL</h3>
+
+      <form align="center">
+        <div id="div">
+          <label for="rol" id="label">Nuevo Rol :</label>
+          <input id="rolText" className="text1" placeholder="Nombre de rol" rows="3" name="rol" value={nombrerol} onChange={limitaralfabeticos} minLength={5} maxLength={20} />
         </div>
-         )
-      }
+        <input type="submit" className="btn btn-primary" /*onClick={this.enviar}*/ value="Agregar Rol" />
+      </form>
+    </div>
+  )
 }
+
 export default CrearRol;
