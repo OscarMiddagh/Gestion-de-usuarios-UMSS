1f9d090134d125f711665b7edbad94447e47ab4a
Alex Castro <alexander72577@gmail.com>
2020-12-11 20:52:13 -0400

    Se añadio los mensajes de advertencia de las resticciones del campo Nombre Rol

--- frontend/src/componentes/Crear-Rol/CrearRol.jsx
+++ frontend/src/componentes/Crear-Rol/CrearRol.jsx
@@ -6,8 +6,11 @@ const CrearRol = () => {
   const [datos, setDatos] = useState({
     nombrerol: ''
   })
+  
+  
   const { nombrerol } = datos
   const limitaralfabeticos = (e) => {
+    document.getElementById("errorRol").style.visibility = "hidden";
     if (e.target.value.match("^[Ñña-zA-Z]*$") != null) {
       setDatos({
         ...datos,
@@ -15,7 +18,8 @@ const CrearRol = () => {
       })
     }
     else {
-      alert("Solo se aceptan caracteres alfabeticos");
+      document.getElementById("errorRol").innerHTML = "Solo se permiten caracteres alfabeticos";
+      document.getElementById("errorRol").style.visibility = "visible";
     }
   }
 
@@ -26,7 +30,8 @@ const CrearRol = () => {
       <form align="center">
         <div id="div">
           <label for="rol" id="label">Nuevo Rol :</label>
-          <input id="rolText" className="text1" placeholder="Nombre de rol" rows="3" name="nombrerol" value={nombrerol} onChange={limitaralfabeticos} minLength={5} maxLength={20} />
+          <input id="nombrerol" className="text1" placeholder="Nombre de rol" rows="3" name="nombrerol" value={nombrerol} onChange={limitaralfabeticos} minLength={5} maxLength={20} required/>
+          <small id="errorRol" className="form-text text-danger" style={{visibility:"hidden"}}></small>
         </div>
         <input type="submit" className="btn btn-primary" /*onClick={this.enviar}*/ value="Agregar Rol" />
       </form>
