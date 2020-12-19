d55043b134141ce2a7b2d696c4433083453e49a7
Andres Moscoso <andres221196@gmail.com>
2020-12-19 09:31:15 -0400

    Correcion en navegabilidad de Crear Rol

--- frontend/src/componentes/Crear-Rol/CrearRol.jsx
+++ frontend/src/componentes/Crear-Rol/CrearRol.jsx
@@ -1,26 +1,27 @@
 //Campo para la historia Crear Rol
 import React from 'react'
 //import { useState } from "react";
+import BarraNav from "../NavBar-Admin/NavBarAdmin";
 import "./CrearRol.css"
 import axios from "axios";
 import {
   Table,
-  
+
   Container,
 } from "reactstrap";
 
 
-const url= "https://gestiondeusuariosumss.herokuapp.com"
-class CrearRol extends React.Component{
-  
+const url = "https://gestiondeusuariosumss.herokuapp.com"
+class CrearRol extends React.Component {
+
   state = {
-    permisos:[],
+    permisos: [],
 
-nombreRol: '',
+    nombreRol: '',
 
 
 
-}
+  }
 
 
   limitaralfabeticos = (e) => {
@@ -37,91 +38,94 @@ nombreRol: '',
     }
   }
 
-  
 
-  componentDidMount(){
+
+  componentDidMount() {
     axios.get(url + "/permisos")
-    .then(response=>{
+      .then(response => {
         console.log(response);
-       this.setState({permisos: response.data})
-   })
-    .catch((error)=>{
+        this.setState({ permisos: response.data })
+      })
+      .catch((error) => {
         console.log(error);
-    });
-   }
+      });
+  }
 
-   crearRol= async (e)=>{
-    await axios.post(url + "/guardarRol",this.state)
-    .then(response=>{
-      console.log(response);
-    })
-  .catch(error=>{
-      console.log(error)
-  })
+  crearRol = async (e) => {
+    await axios.post(url + "/guardarRol", this.state)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 
   //handlerSubmit=(e) =>{
-    // e.preventDefault()
-    //console.log(this.state)
+  // e.preventDefault()
+  //console.log(this.state)
   //}
 
   //setPermisoState=(e)=>{
-    //permisoState.map(d =>{
-      //return{
-        //select: false,
-        //nombrePermiso: d.nombrePermiso,
-      //}
+  //permisoState.map(d =>{
+  //return{
+  //select: false,
+  //nombrePermiso: d.nombrePermiso,
+  //}
 
 
-    //})
+  //})
 
   //}
-  render(){
-    const {nombreRol}=this.state
-  return (
-    <div className="crear_rol" id="div"> <br />
+  render() {
+    const { nombreRol } = this.state
+    return (
+      <div>
+        <BarraNav/>
+        <div className="crear_rol" id="div"> <br />
 
-      <form align="center" id="form" onSubmit={this.crearRol}>
-      <h3 align="center" id="titulo">CREAR ROL</h3>
-        <div id="div">
-          <label htmlFor="rol" id="label">Nuevo Rol :</label>
-          <input id="rolText" className="nombre" placeholder="Nombre de rol" rows="3" name="nombreRol" value={nombreRol} onChange={this.limitaralfabeticos} minLength={5} maxLength={20} />
-        </div>
+          <form align="center" id="form" onSubmit={this.crearRol}>
+            <h3 align="center" id="titulo">CREAR ROL</h3>
+            <div id="div">
+              <label htmlFor="rol" id="label">Nuevo Rol :</label>
+              <input id="rolText" className="nombre" placeholder="Nombre de rol" rows="3" name="nombreRol" value={nombreRol} onChange={this.limitaralfabeticos} minLength={5} maxLength={20} />
+            </div>
 
             <Container>
-        <div id="mensajeEnviado" className="alert alert-primary" role="alert" hidden={true}/>
-          <div className="container">
-            <h1 style={{textAlign:"center"}}>Permisos disponibles</h1>           
-            <div className="justify-content-center align-items-center">
-          <Table>
-            
-              <tbody className="table-dark">
-              {this.state.permisos.map((dato) => (   //por cada dato que se muestre lo siguiente, se debe colocar el nombre de la base de datos 
-                <tr>                 
-                  <td>{dato.nombrePermiso}</td>
-                  <td>
-                    <input type="checkbox" className="btn btn-primary" align="center"/>
-                  
-                  </td>
-                </tr>
-                
-              ))}
-              </tbody>
-             
-          </Table>
-            </div>
-            <div>
-        <button type="submit" className="btn btn-primary" align="center"
-                      //onClick={() => this.registrarse(this.state.form.documentoDeIdentidad)}
-                      >CREAR ROL
-            </button>
-            </div> 
-          </div>
+              <div id="mensajeEnviado" className="alert alert-primary" role="alert" hidden={true} />
+              <div className="container">
+                <h1 style={{ textAlign: "center" }}>Permisos disponibles</h1>
+                <div className="justify-content-center align-items-center">
+                  <Table>
 
-        </Container>
-        </form>
-    </div>
-  )
+                    <tbody className="table-dark">
+                      {this.state.permisos.map((dato) => (   //por cada dato que se muestre lo siguiente, se debe colocar el nombre de la base de datos 
+                        <tr>
+                          <td>{dato.nombrePermiso}</td>
+                          <td>
+                            <input type="checkbox" className="btn btn-primary" align="center" />
+
+                          </td>
+                        </tr>
+
+                      ))}
+                    </tbody>
+
+                  </Table>
+                </div>
+                <div>
+                  <button type="submit" className="btn btn-primary" align="center"
+                  //onClick={() => this.registrarse(this.state.form.documentoDeIdentidad)}
+                  >CREAR ROL
+            </button>
+                </div>
+              </div>
+
+            </Container>
+          </form>
+        </div>
+      </div>
+    )
   }
 }
 
